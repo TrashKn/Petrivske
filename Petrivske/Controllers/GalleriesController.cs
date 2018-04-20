4dbf5969292cc11fe14829fdc3c5f1584e360117
TrashKn <normund@normund.com.ua>
2018-04-20 13:29:52 +0300

    AddGalery

---
+++ Petrivske/Controllers/GalleriesController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Petrivske.Models;
+using System.IO;
+
+namespace Petrivske.Controllers
+{
+    public class GalleriesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult UploadImage()
+        {
+            //Request - вся информация о запросе, пришедшая от клиента (форма, файлы, служебная информация и т.д.)
+            //Request.Files - даёт доступ к файлам, присланным от клиента
+            //Проверяем, есть ли файлы
+            if (Request.Files.Count > 0)
+            {
+                var file = Request.Files[0];
+                //Узнать по какому физическому пути (physical path) находится нужная нам папка, в которую будет сохраняться файл:
+                string path = Server.MapPath("~/GalleryImages/");
+                //string pathfile = path + "\\" + file.FileName;                
+                string filename = Guid.NewGuid() + file.FileName;
+                //Комбинируем путь к папке и имя файла, чтобы получить путь к файлу.
+                string pathname = Path.Combine(path, filename);
+                //Сохраняем файл по полученному пути
+                file.SaveAs(pathname);
+                return Json(filename);
+            }
+            return Json("error");
+        }
+
+        // GET: Galleries
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View(db.Galleries.ToList());
+        }
+
+        // GET: Galleries/Details/5
+        [Authorize]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // GET: Galleries/Create
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Galleries/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,Title")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Galleries.Add(gallery);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(gallery);
+        }
+
+        // GET: Galleries/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: Galleries/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include = "Id,Title")] Gallery gallery)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(gallery).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(gallery);
+        }
+
+        // GET: Galleries/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Gallery gallery = db.Galleries.Find(id);
+            if (gallery == null)
+            {
+                return HttpNotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: Galleries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Gallery gallery = db.Galleries.Find(id);
+            db.Galleries.Remove(gallery);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
